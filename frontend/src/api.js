8acb0a5797d9a6be8cbe6495064b78185667e3cc
yush-1111 <yush@1111>
2026-03-27 17:58:35 +0530

    update all

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://home-aseesment-2.onrender.com"
+  baseURL: 'http://localhost:5000/api'
 });
 
 api.interceptors.request.use((config) => {
