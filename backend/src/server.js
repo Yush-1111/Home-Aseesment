8573680d883b742f2346b6ea0c769df4e558d5c4
yush-1111 <yush@1111>
2026-03-27 18:46:21 +0530

    update all

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,23 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://home-aseesment.vercel.app"
+];
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173'
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
