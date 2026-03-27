0d5f0d17849a4e552f59932623f5316a32eb2387
yush-1111 <yush@1111>
2026-03-27 17:34:39 +0530

    update all

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -19,7 +19,9 @@ const userSchema = new mongoose.Schema(
       required: true
     }
   },
-  { timestamps: true }
+  {
+    timestamps: true
+  }
 );
 
 module.exports = mongoose.model('User', userSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: 'http://localhost:5173'
+    origin: 'http://localhost:5173,https://home-aseesment.vercel.app/login'
   })
 );
 app.use(express.json());
